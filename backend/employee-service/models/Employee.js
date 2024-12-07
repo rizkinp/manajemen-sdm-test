77dc75458eab0623ce97fd7c6f72deffd20f5deb
rizkinp <rizkinp87@gmail.com>
2024-12-07 23:31:32 +0700

    fix/employee: fixing models

--- backend/employee-service/models/Employee.js
+++ backend/employee-service/models/Employee.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const employeeSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    position: { type: String, required: true },
     password: { type: String, required: true },
 }, { timestamps: true });
 
