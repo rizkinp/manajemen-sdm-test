259a141805528e293ac919210e4c703820c6b786
rizkinp <rizkinp87@gmail.com>
2024-12-08 00:30:59 +0700

    fix/be&fe: fixing bcrypt conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import AuthProvider from "./contexts/AuthProvider";
-import Loader from "./components/Loader";
+import Loader from "./components/loader";
 import "./App.css";
 import NotFoundPage from "./pages/NotFoundPage";
 const LoginPage = React.lazy(() => import("./pages/LoginPage"));
